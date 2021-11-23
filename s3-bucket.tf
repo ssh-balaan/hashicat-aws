ba0e7aa23fb75ae17733ed9e558a1f61d0ed8a4c
SSH <soohyun.song@balaan.co.kr>
2021-11-23 09:52:46 +0000

    mod

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
-module "s3-bucket" {
-  source  = "app.terraform.io/ssh-train/s3-bucket/aws"
-  version = "2.2.0"
-  # insert required variables here
-  bucket_prefix = "ssh"
-}
+// module "s3-bucket" {
+//   source  = "app.terraform.io/ssh-train/s3-bucket/aws"
+//   version = "2.2.0"
+//   # insert required variables here
+//   bucket_prefix = "ssh"
+// }
